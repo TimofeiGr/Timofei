6030135bad96c371090d9674a2a2a0a3b800c786
Timofei <timofeiesi@gmail.com>
2026-03-10 23:40:52 +0300

    Добавил сервер и клиент

--- client.py
+++ client.py
@@ -1,11 +1,1 @@
-import socket
-
-def send(cmd):
-    with socket.socket() as s:
-        s.connect(('127.0.0.1', 65432))
-        s.send(cmd.encode())
-        return s.recv(1024).decode()
-
-if __name__ == "__main__":
-    print(send("SET username Timofei"))
-    print(send("GET username"))
+# клиент
